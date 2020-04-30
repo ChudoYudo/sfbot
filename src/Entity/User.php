1b8cb0cd8d06e1bd6292d851e1b5a16a530ea796
Raman <yudo.raman@sadgroup.com>
2020-04-30 18:02:47 +0300

    add user entity

---
+++ src/Entity/User.php
@@ -0,0 +1,57 @@
+<?php
+
+namespace App\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity(repositoryClass="App\Repository\UserRepository")
+ */
+class User
+{
+    /**
+     * @ORM\Id()
+     * @ORM\GeneratedValue()
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="string", length=255, nullable=true)
+     */
+    private $userName;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $chatId;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getUserName(): ?string
+    {
+        return $this->userName;
+    }
+
+    public function setUserName(?string $userName): self
+    {
+        $this->userName = $userName;
+
+        return $this;
+    }
+
+    public function getChatId(): ?string
+    {
+        return $this->chatId;
+    }
+
+    public function setChatId(string $chatId): self
+    {
+        $this->chatId = $chatId;
+
+        return $this;
+    }
+}
